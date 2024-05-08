4ce7964fcb78d12c51ff08411e2b5d2abc8063b1
Jhoelber <116396136+Jhoelber@users.noreply.github.com>
2024-05-08 08:18:14 -0300

    Update PaginaLogin.tsx

--- src/renderer/src/components/PaginaLogin.tsx
+++ src/renderer/src/components/PaginaLogin.tsx
@@ -25,7 +25,6 @@ export const PaginaLogin = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-
     if (username === 'user' && password === 'user') {
       setIsLoggedIn(true);
     } else {
@@ -38,16 +37,16 @@ export const PaginaLogin = () => {
       {isLoggedIn ? (
         <PaginaPrincipal />
       ) : (
-        <div className="grid grid-cols-2 ">
 
+        <div className="grid grid-cols-2 ">
           <div className="flex flex-col items-center" >
-            <img src={cld.image(`imagesLogin/icon`).toURL()} className="mr-auto " alt="Icone logo da cidade" />
+            <img src={cld.image(`imagesLogin/icon`).toURL()} className="mr-auto p-2" alt="Icone logo da cidade" />
             <h1 className="text-3xl font-bold text-zinc-800">TOTEM DE AUTO ATENDIMENTO</h1>
             <h2 className="text-3xl font-bold text-zinc-800">SALA DO EMPREENDEDOR</h2>
 
             <div className="flex items-center gap-10">
               <img src={cld.image(`imagesLogin/mulherTotem`).toURL()} className="w-86 h-96" alt="Mulher mexendo no totem" />
-              <div className="flex-col p-10">
+              <div className="flex-col pr-10 pl-10 pt-10">
                 <img src={cld.image(`imagesLogin/Parcelasimplesnacional`).toURL()} className="w-32 animate-bounce " alt="Icone parcela Naciol" />
                 <img src={cld.image(`imagesLogin/Imprimirboleto`).toURL()} className="w-32 animate-bounce2" alt="Icone Imprimir boleto das" />
                 <img src={cld.image(`imagesLogin/Formalizacaomei`).toURL()} className="w-32 animate-bounce" alt="Icone formalização mei" />
@@ -55,15 +54,16 @@ export const PaginaLogin = () => {
                 <img src={cld.image(`imagesLogin/Debitoautomatico`).toURL()} className="w-32 animate-bounce" alt="Icone debito automatico" />
               </div>
             </div>
+
             <div>
 
             </div>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-white col-span-1 h-screen">
+          <div className="flex flex-col items-center justify-center bg-white col-span-1 h-screen ">
             <img src={cld.image(`imagesLogin/iconLogin3`).toURL()} alt="" className="w-96 pb-4" />
-            {/* <h2 className="pb-24 text-3xl font-bold text-zinc-800">BEM VINDO AO TOTEM</h2> */}
-
+            {/* <h2 className="pb-24 text-3xl font-bold text-zinc-800">BEM VINDO AO TOTEM</h2>  verificar oq fica
+            melhor, imagem ou texto*/}
 
             <form className="flex flex-col gap-2">
               <label htmlFor="login" className="text-3xl">Telefone</label>
@@ -87,22 +87,24 @@ export const PaginaLogin = () => {
                 className="w-56 rounded-md bg-[#DBEAFE] h-8 p-2"
                 placeholder="Digite sua senha"
               />
+
               <input
                 type="submit"
                 value="ENTRAR"
                 className="w-56 bg-sky-400 rounded-md mt-2 text-white p-1"
                 onClick={handleLogin}
               />
-            </form>
 
+            </form>
             <div className={`${corretLogin} text-red-500`} >
               <h1>Usuario ou senha incorreto! </h1>
             </div>
+
             <div className="flex flex-col items-end ml-20">
               <p className="text-sky-400"><a href="https://www.google.com.br/?hl=pt-BR">Esqueci minha senha</a></p>
               <p className="text-sky-400"><a href="#">alterar dados</a> </p>
-
             </div>
+
             <p><a href="#">Ainda nao possui conta? clique em <span className="text-sky-400">Cadastrar</span></a></p>
           </div>
         </div>
