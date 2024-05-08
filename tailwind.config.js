29c987346f0d29e83a874dd2d8eaf4a24c829fbe
Jhoelber <116396136+Jhoelber@users.noreply.github.com>
2024-05-08 08:03:53 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'bounce': {
+          '0%, 100%': { transform: 'translateY(-1)' }, // Posição inicial e final
+          '30%': { transform: 'translateY(2px)' },   // Pico da animação
+          '50%': { transform: 'translateY(4px)' },   // Meio da animação, deslocamento maior
+          '70%': { transform: 'translateY(2px)' },   // Retrocesso do pico
+          '100%': { transform: 'translateY(0)' },
+        },
+        'bounce2': {
+          '0%, 100%': { transform: 'translateY(0)' }, // Posição inicial e final
+          '30%': { transform: 'translateY(2px)' },   // Pico da animação
+          '50%': { transform: 'translateY(4px)' },   // Meio da animação, deslocamento maior
+          '70%': { transform: 'translateY(2px)' },   // Retrocesso do pico
+          '100%': { transform: 'translateY(0)' },
+        }
+
+      },
+      animation: {
+        'bounce': 'bounce 2s ease-in-out infinite',
+        'bounce2': 'bounce2 3s ease-in-out infinite'
+      }
+
+
+    },
   },
   plugins: [],
 }
