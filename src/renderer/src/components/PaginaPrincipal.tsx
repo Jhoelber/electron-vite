c33bebe8c208a273978229321cf95fa80932915a
Jhoelber <116396136+Jhoelber@users.noreply.github.com>
2024-05-09 11:36:42 -0300

    Update PaginaPrincipal.tsx

--- src/renderer/src/components/PaginaPrincipal.tsx
+++ src/renderer/src/components/PaginaPrincipal.tsx
@@ -1,21 +1,19 @@
+import { AdvancedImage } from "@cloudinary/react";
 import { Cloudinary } from "@cloudinary/url-gen";
-import { Topo } from "./Topo";
 import { UrlImagesData } from "@renderer/data/UrlImagesData";
 import { Icons } from "./Icons";
-import { AdvancedImage } from "@cloudinary/react";
+import { Topo } from "./Topo";
 
 export const PaginaPrincipal = () => {
   const cld = new Cloudinary({
     cloud: {
       cloudName: "dfgbasaxa"
     }
-
   })
   const myImage = cld.image(`images/LogoIcon.png`);
 
   return (
     <div className="bg-white " >
-
       <Topo />
 
       <div className="grid grid-cols-1 gap-6 place-items-center md:grid-cols-2 lg:grid-cols-3 md:gap-32 md:pb-10 " >
@@ -32,15 +30,12 @@ export const PaginaPrincipal = () => {
         ))}
       </div>
 
-
       <footer className="flex h-12 bg-[#014A7D] justify-center items-center py-2 text-white font-light text-xs">
         <AdvancedImage className="m-2 max-h-10" cldImg={myImage} alt="icone do rodapé" />
         <img className="m-2 max-h-10" src={`${myImage}`} alt="" />
         <p>Prefeitura Municipal de jacarezinho | <code> Jhoelber - Kellton</code> </p>
       </footer>
-
     </div>
-
 
   )
 }
